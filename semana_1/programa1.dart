7170eec6d12cb2ce8fc523ea0de5a4d9510b1d2d
Hilda_Hernandez_Guerrero <>
2021-05-25 21:25:55 +0000

    Ultimas modificaciones de  Programas De Semana1

--- semana_1/programa1.dart
+++ semana_1/programa1.dart
@@ -1,4 +1,5 @@
 //Calcular el area de un Triangulo
+//
 void main(){
    double resultado = area(7,13);
    print("El area del Triangulo es: $resultado");
--- semana_1/programa2.dart
+++ semana_1/programa2.dart
@@ -1,5 +1,5 @@
 //Calcular el perimetro de un hexagono
-//Para obtener el Perimetro=n.lados * lado
+//Para obtener el Perimetro del Hexagonose realiza la operacion =n.lados * lado
 
 void main(){
    int resultado = perimetro(6,5);
